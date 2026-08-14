345811782c6c5a9fc47b9a5babcbf2d4fdaba018
Priyash Sharma <priyasharma496@gmail.com>
2026-08-14 20:28:21 +0530

    Kadane's Algorithm/

---
+++ Kadane's Algorithm/LC53_Maximum_Subarray.java
@@ -0,0 +1,23 @@
+/*Leetcode: 53. Maximum Subarray
+Link: https://leetcode.com/problems/maximum-subarray/  */
+
+class Solution {
+    public int maxSubArray(int[] nums) 
+    {
+        int currentSum=nums[0];
+        int maxSum=nums[0];
+        for (int i=1;i<nums.length;i++)
+        {
+            if (currentSum+nums[i]<nums[i])
+            {
+                currentSum=nums[i];
+            }
+            else
+            {
+                currentSum+=nums[i];
+            }
+            maxSum=Math.max(maxSum,currentSum);
+        }
+        return maxSum;
+    }
+}
